5b6c954db6c9c66fd478509e4d070b9a4fc9cbee
Ana Beatriz <abeatriz.fernandes@outlook.com>
2025-05-26 18:33:41 -0300

    atualizando paineis admin, aluno e monitor

--- connect-frontend/src/services/api.js
+++ connect-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fatecconnect-backend.onrender.com/api",
 });
 //https://fatecconnect-backend.onrender.com
 //http://localhost:5000/api
