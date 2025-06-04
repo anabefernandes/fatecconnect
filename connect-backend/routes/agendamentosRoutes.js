e7dc86c97eb1b786ec66e05f88cf0404cb1bfe24
Ana Beatriz <abeatriz.fernandes@outlook.com>
2025-06-04 16:59:03 -0300

    salvando alterações

--- connect-backend/routes/agendamentosRoutes.js
+++ connect-backend/routes/agendamentosRoutes.js
@@ -1,48 +1,63 @@
 const express = require("express");
 const Agendamento = require("../models/Agendamento");
-const verificarToken = require("../middlewares/verificarToken"); 
-const mongoose = require('mongoose');
+const verificarToken = require("../middlewares/verificarToken");
+const mongoose = require("mongoose");
 const router = express.Router();
 
-router.post('/agendar-monitoria', async (req, res) => {
+//agendar monitoria
+router.post("/agendar-monitoria", async (req, res) => {
   try {
     const { alunoId, monitorId, data } = req.body;
 
     if (!alunoId || !monitorId || !data) {
-      return res.status(400).json({ mensagem: 'Dados incompletos para agendamento' });
+      return res
+        .status(400)
+        .json({ mensagem: "Dados incompletos para agendamento" });
     }
 
     // Verifica se IDs são válidos
-    if (!mongoose.Types.ObjectId.isValid(alunoId) || !mongoose.Types.ObjectId.isValid(monitorId)) {
-      return res.status(400).json({ mensagem: 'IDs inválidos' });
+    if (
+      !mongoose.Types.ObjectId.isValid(alunoId) ||
+      !mongoose.Types.ObjectId.isValid(monitorId)
+    ) {
+      return res.status(400).json({ mensagem: "IDs inválidos" });
     }
 
     const dataObj = new Date(data);
     if (isNaN(dataObj.getTime())) {
-      return res.status(400).json({ mensagem: 'Data inválida' });
+      return res.status(400).json({ mensagem: "Data inválida" });
     }
 
     // Verifica conflito
     const conflito = await Agendamento.findOne({
       monitor: monitorId,
-      data: dataObj
+      data: dataObj,
     });
 
     if (conflito) {
-      return res.status(400).json({ mensagem: 'Horário indisponível para este monitor.' });
+      return res
+        .status(400)
+        .json({ mensagem: "Horário indisponível para este monitor." });
     }
 
     const novoAgendamento = await Agendamento.create({
       aluno: alunoId,
       monitor: monitorId,
       data: dataObj,
-      status: 'pendente'
+      status: "pendente",
     });
 
-    res.status(201).json({ mensagem: 'Agendamento criado com sucesso', agendamento: novoAgendamento });
+    res
+      .status(201)
+      .json({
+        mensagem: "Agendamento criado com sucesso",
+        agendamento: novoAgendamento,
+      });
   } catch (err) {
-    console.error('Erro no agendamento:', err);
-    res.status(500).json({ mensagem: 'Erro ao agendar monitoria', erro: err.message });
+    console.error("Erro no agendamento:", err);
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao agendar monitoria", erro: err.message });
   }
 });
 
@@ -61,10 +76,28 @@ router.patch("/agendamentos/:id/status", verificarToken, async (req, res) => {
 
     return res.status(200).json({ success: true, agendamento });
   } catch (err) {
-    console.error("Erro ao atualizar status:", err);  // <- aqui
+    console.error("Erro ao atualizar status:", err); // <- aqui
     return res.status(500).json({ erro: "Erro ao atualizar status" });
   }
 });
 
+router.get("/agendamentos/aluno", verificarToken, async (req, res) => {
+  try {
+    if (req.user.papel !== "aluno") {
+      return res
+        .status(403)
+        .json({ erro: "Acesso permitido apenas para alunos." });
+    }
+
+    const agenda = await Agendamento.find(
+      { aluno: req.user.id },
+      { senha: 0 }
+    ).populate("monitor", "nome");
+
+    return res.status(200).json({ success: true, agenda });
+  } catch (err) {
+    return res.status(500).json({ erro: "Erro ao buscar agendamentos." });
+  }
+});
 
 module.exports = router;
--- connect-backend/routes/usuarioRoutes.js
+++ connect-backend/routes/usuarioRoutes.js
@@ -6,7 +6,6 @@ const Curso = require("../models/Curso");
 const verificarToken = require("../middlewares/verificarToken");
 const verificarAdmin = require("../middlewares/verificarAdmin");
 const sendEmail = require("../mail");
-
 let resetTokens = {};
 const router = express.Router();
 const multer = require("multer");
@@ -73,25 +72,6 @@ router.post(
     }
   }
 );
-
-router.get("/agendamentos/aluno", verificarToken, async (req, res) => {
-  try {
-    if (req.user.papel !== "aluno") {
-      return res
-        .status(403)
-        .json({ erro: "Acesso permitido apenas para alunos." });
-    }
-
-    const agenda = await Agendamento.find(
-      { aluno: req.user.id },
-      { senha: 0 }
-    ).populate("monitor", "nome");
-
-    return res.status(200).json({ success: true, agenda });
-  } catch (err) {
-    return res.status(500).json({ erro: "Erro ao buscar agendamentos." });
-  }
-});
 
 // cadastro de usuário comum (aluno)
 router.post("/cadastro", async (req, res) => {
